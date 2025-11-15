d6587796958eef4fde279710f243e075d51a5c9d
G-Chist <komp.lord@gmail.com>
2025-11-15 14:27:06 -0500

    add default lists

--- appinit.py
+++ appinit.py
@@ -17,15 +17,11 @@ interests = ["ML", "HPC", "AI", "Cybersecurity", "KDD", "Graphics"]
 languages = ["C", "C++", "Python", "Haskell", "Java", "JavaScript", "Lisp", "Rust"]
 courses = ["CS1004", "CS2011", "CS2102", "CS2201", "CS3013", "CS3133", "CS3431", "CS3516", "CS3733", "CS4233", "CS4001", "CS4002", "CS4003"]
 
-@app.before_request
-def initDB(*args, **kwargs):
-    if app._got_first_request:
-        db.create_all()
+
 
 # fill in db with some things
 
-@sqla.event.listens_for(Major.__table__, 'after_create')
-def add_majors(*args, **kwargs):
+def add_majors():
     query = sqla.select(Major)
     if db.session.scalars(query).first() is None:
         majorsDict = [{"name":majors[i]} for i in range(len(majors))]
@@ -34,8 +30,7 @@ def add_majors(*args, **kwargs):
             db.session.add(Major(name=t["name"]))
         db.session.commit()
 
-@sqla.event.listens_for(ResearchTopic.__table__, 'after_create')
-def add_interests(*args, **kwargs):
+def add_interests():
     query = sqla.select(ResearchTopic)
     if db.session.scalars(query).first() is None:
         interestsDict = [{"name":interests[i]} for i in range(len(interests))]
@@ -43,8 +38,7 @@ def add_interests(*args, **kwargs):
             db.session.add(ResearchTopic(name=t["name"]))
         db.session.commit()
 
-@sqla.event.listens_for(Language.__table__, 'after_create')
-def add_languages(*args, **kwargs):
+def add_languages():
     query = sqla.select(Language)
     if db.session.scalars(query).first() is None:
         languagesDict = [{"name":languages[i]} for i in range(len(languages))]
@@ -52,14 +46,24 @@ def add_languages(*args, **kwargs):
             db.session.add(Language(name=t["name"]))
         db.session.commit()
 
-@sqla.event.listens_for(Course.__table__, 'after_create')
-def add_courses(*args, **kwargs):
+def add_courses():
     query = sqla.select(Course)
     if db.session.scalars(query).first() is None:
         coursesDict = [{"name":courses[i], "coursenum": courses[i]} for i in range(len(courses))]
         for t in coursesDict:
             db.session.add(Course(name=t["name"], coursenum=t["coursenum"]))
         db.session.commit()
+
+@app.cli.command("init-db")
+def init_db():
+    """Clear the existing data and create new tables."""
+    db.drop_all()
+    db.create_all()
+    add_majors()
+    add_interests()
+    add_languages()
+    add_courses()
+    print("Initialized the database.")
 
 
 if __name__ == "__main__":
